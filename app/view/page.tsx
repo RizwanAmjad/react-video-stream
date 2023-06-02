f11e8677ee46f6c348ff85ee70e27e4b6a9c1f86
RizwanAmjad <rizwanamjadms@gmail.com>
2023-06-02 16:40:12 +0500

    created the app with millicast

---
+++ app/view/page.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function ViewPage() {
+  return (
+    <iframe src="https://viewer.millicast.com?streamId=vNud3K/myStreamName"></iframe>
+  )
+}
+
+export default ViewPage
